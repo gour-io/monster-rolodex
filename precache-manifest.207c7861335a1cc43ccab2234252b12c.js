bce2b343ac6182e99b009afcfeb92da10e8811b1
gour-io <you@example.com>
2020-04-01 15:25:49 +0530

    Updates

---
+++ precache-manifest.207c7861335a1cc43ccab2234252b12c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "16733c758e8f23318817e48081e0ffd3",
+    "url": "/monster-rolodex/index.html"
+  },
+  {
+    "revision": "c6d6075ab38d9437b4f5",
+    "url": "/monster-rolodex/static/css/main.654101ed.chunk.css"
+  },
+  {
+    "revision": "147f1327e7798733efc4",
+    "url": "/monster-rolodex/static/js/2.d562cfe4.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monster-rolodex/static/js/2.d562cfe4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c6d6075ab38d9437b4f5",
+    "url": "/monster-rolodex/static/js/main.688fbac8.chunk.js"
+  },
+  {
+    "revision": "180f3ef3e47bfbea4f20",
+    "url": "/monster-rolodex/static/js/runtime-main.06495f93.js"
+  }
+]);
